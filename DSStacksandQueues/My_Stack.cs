9582ec4f987694ff3f9e246c78a12ea1ab85cc1c
Niraj Suryavanshi <poojakadamlatur@gmail.com>
2022-05-07 17:02:19 +0530

    createQueue

---
+++ DSStacksandQueues/My_Stack.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+namespace DSStacksandQueues
+{/// <summary>
+/// Template for testing stack and queue
+/// </summary>
+
+    public class My_Queue<Gtype>
+    {/// <summary>
+     /// UC3 - create a Queue  
+     /// </summary
+
+        Node<Gtype> front;
+        Node<Gtype> rare;
+
+
+        public void Enqueue(Gtype data)
+        {
+            //creating node with given data
+            Node<Gtype> node = new Node<Gtype>(data);
+
+            //if the given element is first element
+            //front element = rare element
+            //we can check anyone either front or rare
+            if (rare is null)
+            {
+                front = node;
+                rare = node;
+            }
+            else
+            {
+                rare.next = node;
+                rare = node;
+            }
+        }
+
+        public void show()
+        {
+            //Created a temp variable
+            Node<Gtype> temp = front;
+            //traverse from front to last element and print element
+            if (temp is null) Console.WriteLine("Queue is Empty");
+            else
+            {
+                while (temp.next != null)
+                {
+                    Console.Write(temp.value + " ");
+                    temp = temp.next;
+                }
+                //to print the last value
+                Console.WriteLine(temp.value);
+            }
+        }
+
+    }
+}
---
+++ DSStacksandQueues/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DSStacksandQueues
+{/// <summary>
+/// 
+/// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            My_Queue<int> my_Queue = new My_Queue<int>();
+
+            my_Queue.Enqueue(56);
+            my_Queue.Enqueue(30);
+            my_Queue.Enqueue(70);
+            my_Queue.show();
+
+
+
+
+        } 
+    }
+}
