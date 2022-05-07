bb9d13acee6829a66ce15548f5924d0f6c6d14fb
Niraj Suryavanshi <poojakadamlatur@gmail.com>
2022-05-07 16:49:14 +0530

    CreateContact

---
+++ DSStacksandQueues/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DSStacksandQueues
+{/// <summary>
+/// 
+/// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            My_Stack<int> my_Stack = new My_Stack<int>();
+
+            my_Stack.push(70);
+            my_Stack.push(30);
+            my_Stack.push(56);
+            my_Stack.Show();
+        } 
+    }
+}
